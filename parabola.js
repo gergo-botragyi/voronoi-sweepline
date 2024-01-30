c3fbddb20faf7023ce96b868f11085df227eed95
botragyi-gergo <botragyi.gergo@gmail.com>
2024-01-30 13:06:23 +0100

    what is happening

--- parabola.js
+++ parabola.js
@@ -1,7 +1,8 @@
 class Parabola{
     constructor(f, p){
-        this.f = f;
+        this.f = f;        
         this.p = p;
+        this.t = this.f.y*2+this.p;
         this.a;
         this.b;
         this.c;
@@ -13,7 +14,7 @@ class Parabola{
     calcPoints(){
         this.a = -1 / (2*this.p)
         this.b = -1*this.a*2*this.f.x;
-        this.c = this.a*(this.f.x**2)+this.f.y
+        this.c = this.a*(this.f.x**2)+this.t
         this.x1 = (-this.b+Math.sqrt(this.b**2 - 4*this.a*this.c))/(2*this.a);
         this.x2 = (-this.b-Math.sqrt(this.b**2 - 4*this.a*this.c))/(2*this.a);
     }
@@ -21,15 +22,19 @@ class Parabola{
     draw(){
         this.calcPoints();        
         let bezier = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        bezier.setAttribute('d', `M ${this.x1} 0 Q ${this.f.x} ${this.f.y*2} ${this.x2} 0`);
+        bezier.setAttribute('d', `M ${this.x1} 0 Q ${this.f.x} ${this.t} ${this.x2} 0`);
         bezier.setAttribute('stroke', "black");
         bezier.setAttribute("fill", "transparent");
         return bezier;
     }
 
-    update(){
+    update(){        
         this.p = sweepline.y-this.f.y;
+        this.t = this.f.y*2+this.p
+        //console.log(sweepline.y+" "+this.f.y)
+        console.log(this.p)
         this.calcPoints();
-        this.svgo.setAttribute('d', `M ${this.x1} 0 Q ${this.f.x} ${this.f.y*2+this.p} ${this.x2} 0`);
+        this.svgo.setAttribute('d', `M ${this.x1} 0 Q ${this.f.x} ${this.t} ${this.x2} 0`);
+        console.log(this.a+" "+this.b+" "+this.c)
     }
 }
